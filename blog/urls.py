9b2913f8c8be1df86940f4923936a70f74b2f407
vermanil <anilrajverma1996@gmail.com>
2017-10-27 20:17:09 +0530

    LogIn done

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.welcomePage, name="welcomePage"),
     url(r'^login', views.loginForm, name="login"),
-    url(r'^register', views.registerForm, name="register")
+    url(r'^register', views.registerForm, name="register"),
+    url(r'^registration', views.registration, name="registration"),
+    url(r'^doLogin', views.Login, name="Login")
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import candidateLoginForm, RegistrationForm
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def welcomePage(request):
@@ -9,32 +10,46 @@ def welcomePage(request):
 
 def loginForm(request):
     form = candidateLoginForm()
-    return render(request, "Login.html", {'form': form, 'name':'Anil'})
+    return render(request, "Login.html", {'form': form})
 
 def registerForm(request):
     form = RegistrationForm()
-    return render(request, "Register.html", {'form': form, 'name': 'Anil'})
+    return render(request, "Register.html", {'form': form})
+
+def Login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            url = "/blog"
+            return HttpResponse("Login Done")
+        else:
+            form = candidateLoginForm()
+            return render(request, "Login.html", {'form': form})
 
 def registration(request):
-    def candidateRegister(request, name):
-        if request.method == 'POST':
-            form = RegistrationForm(request.POST)
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
 
-            if form.is_valid():  # invoke .is_valid
-                username = form.cleaned_data['username']
-                alreadyUser = User.objects.filter(username=username)
-                    # print(alreadyUser.count())
-                if (alreadyUser.count() != 0):
-                    form = RegistrationForm()
-                    return render(request, "reg.html", {'form': form, "name": name, 'stat': "User already exist"})
-                else:
-                    user = User.objects.create_user(
-                        username=form.cleaned_data['username'],
-                        email=form.cleaned_data['email'],
-                        password=form.cleaned_data['password']
-                    )
-                    user.first_name = form.cleaned_data['first_name']
-                    user.last_name = form.cleaned_data['last_name']
-                    user.save()
-                    form = candidateLoginForm()
-                    return render(request, "Login.html", {'form': form, "name": name})
+        if form.is_valid():  # invoke .is_valid
+            username = form.cleaned_data['username']
+            alreadyUser = User.objects.filter(username=username)
+                # print(alreadyUser.count())
+            if (alreadyUser.count() != 0):
+                form = RegistrationForm()
+                return render(request, "reg.html", {'form': form, "name": name, 'stat': "User already exist"})
+            else:
+                user = User.objects.create_user(
+                    username=form.cleaned_data['username'],
+                    email=form.cleaned_data['email'],
+                    password=form.cleaned_data['password']
+                )
+                user.first_name = form.cleaned_data['first_name']
+                user.last_name = form.cleaned_data['last_name']
+                user.save()
+                form = candidateLoginForm()
+                return render(request, "Login.html", {'form': form})
+        else:
+            return HttpResponse("Form is invalid")
